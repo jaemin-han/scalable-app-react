a5229a52e8c717cdcf9a6711624f11eb45a488d2
jaemin.han <jaemin.han@nyu.edu>
2018-07-29 17:59:39 -0400

    get data from the server and flowed onto redux state using sagas

--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -9,12 +9,19 @@ import React from 'react';
 
 import styles from './styles.css';
 
-function Navigation() {
+function Navigation({ topics }) {
   return (
     <div className={styles.navigation}>
-    This is the navigation component
+    We have {topics.length} topics in the navigation components
     </div>
   );
 }
 
+Navigation.propTypes = {
+  topics: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+
+    })
+  )
+}
 export default Navigation;
