52c4be810902dbfb0574a6c018cee32cb929d251
jaemin.han <jaemin.han@nyu.edu>
2018-07-30 16:44:30 -0400

    store data from the server on the redux store

--- app/containers/LinkListContainer/sagas.js
+++ app/containers/LinkListContainer/sagas.js
@@ -1,7 +1,9 @@
 // import { take, call, put, select } from 'redux-saga/effects';
-import { call } from 'redux-saga/effects';
+import { call, put } from 'redux-saga/effects';
 import { takeLatest } from 'redux-saga';
 import { SELECT_TOPIC } from '../NavigationContainer/constants';
+import { requestLinksSucceeded, requestLinksFailed } from './actions';
+import { request } from 'https';
 
 function fetchLinksFromServer(topic) {
   return fetch(`http://localhost:3000/api/topics/${topic.name}/links`)
@@ -11,10 +13,10 @@ function fetchLinksFromServer(topic) {
 function* fetchLinks(action) {
   try {
     const links = yield call(fetchLinksFromServer, action.topic);
-    // dispatch action to store links
-    console.log('LINKS FROM SERVER:', links);
+    yield put(requestLinksSucceeded(links));
+    // console.log('LINKS FROM SERVER:', links);
   } catch (e) {
-    // dispatch action to store error
+    yield put(requestLinksFailed(e.message));
   }
 }
 
